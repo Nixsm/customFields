fbd02ea7fa65c6bc2ea7ca8f7fd55162a05809cd
Nixsm <nichsmm@gmail.com>
2017-01-03 22:19:46 -0200

    Adding login routes and model resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create"
+  delete '/login', to: "sessions#destroy"
+
+  resources :users do
+    resources :fields
+    resources :contacts
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
