0d69cb4aefa90ce20b2cc19c299a8dd51f3c89e2
Nixsm <nichsmm@gmail.com>
2017-01-03 23:08:31 -0200

    Authenticating, currently not using any encrypton (BAD)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(name: params[:session][:name].downcase)
+    if user && user.pass == params[:session][:pass]
+      log_in user
+      redirect_to user and return
+    end
 
+    render 'new'
   end
 
   def destroy
