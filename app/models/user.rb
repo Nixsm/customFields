54cd4e434fcee3d5f6313d5c92b25ff316d59f83
Nixsm <nichsmm@gmail.com>
2017-01-04 19:39:25 -0200

    refactoring users to contain a hash of fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User
   include Mongoid::Document
   field :name, type: String
   field :pass, type: String
+  field :custom_fields, type: Hash
 
-  embeds_many :custom_fields
   embeds_many :contacts
 end
