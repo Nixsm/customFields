617b936572d066ffc2ffa1e8919c4e0565c5af87
Nixsm <nichsmm@gmail.com>
2017-01-05 17:42:47 -0200

    Displaying form to add new contacts based on custom fields

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,8 +3,25 @@ class ContactsController < ApplicationController
   end
 
   def new
+    @user = User.find(params[:user_id])
+
   end
 
   def create
+    @user = User.find(params[:user_id])
+
+    @user.contacts ||=
+
+    contact_params = params[:contact]
+
+    contact = Contact.new
+
+    contact_params.each do |param, value|
+      contact[param] = value
+    end
+
+
+
+    render plain: contact.inspect
   end
 end
